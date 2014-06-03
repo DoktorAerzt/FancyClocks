00ce8440eb5f5cb42635c74e3483f503903f7edf
Empty2k12 <11deutron11@gmail.com>
2014-06-03 16:34:16 +0200

    Cleanupz

--- src/main/java/de/empty2k12/fancyclocks/FancyClocks.java
+++ src/main/java/de/empty2k12/fancyclocks/FancyClocks.java
@@ -4,8 +4,6 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.gen.structure.MapGenStructureIO;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -14,14 +12,12 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.VillagerRegistry;
 import de.empty2k12.fancyclocks.common.block.ClockBlocks;
 import de.empty2k12.fancyclocks.common.item.ClockItems;
 import de.empty2k12.fancyclocks.common.misc.Config;
 import de.empty2k12.fancyclocks.common.misc.ModInfo;
 import de.empty2k12.fancyclocks.common.misc.TabFancyClocks;
 import de.empty2k12.fancyclocks.common.proxy.CommonProxy;
-import de.empty2k12.fancyclocks.common.world.gen.village.ComponentHorologistsShop;
 import de.empty2k12.fancyclocks.common.world.gen.village.VillageHandlerHorologist;
 
 @Mod(modid = ModInfo.MOD_ID, version = ModInfo.MOD_VERSION, name = ModInfo.MOD_NAME)
@@ -35,16 +31,13 @@ public class FancyClocks {
 
 	public static CreativeTabs tabFancyClocks = new TabFancyClocks("FancyClocks");
 
-	//TODO: everything from my todo list!
-
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		Config.init(event.getSuggestedConfigurationFile());
 		ClockBlocks.init();
 		ClockItems.init();
 		VillageHandlerHorologist.init();
-		proxy.registerRenderers();
-		addRecipes();
+		FancyClocks.init();
 	}
 
 	@EventHandler
@@ -62,5 +55,10 @@ public class FancyClocks {
 		GameRegistry.addRecipe(new ItemStack(ClockBlocks.clock, 1), "xox", "xlx", "xix", 'x', new ItemStack(Blocks.planks, 1), 'o', new ItemStack(Items.clock, 1), 'l', new ItemStack(Items.stick), 'i', new ItemStack(Items.iron_ingot, 1));
 		GameRegistry.addRecipe(new ItemStack(ClockBlocks.clock, 1), "sss", "sos", "sss", 'o', new ItemStack(Items.clock, 1), 's', new ItemStack(Blocks.stone));
 		GameRegistry.addRecipe(new ItemStack(ClockItems.screwdriver, 1), "l", "l", "x", 'x', new ItemStack(Items.iron_ingot, 1), 'l', new ItemStack(Items.stick));
+	}
+
+	public static void init() {
+		proxy.registerRenderers();
+		addRecipes();
 	}
 }
